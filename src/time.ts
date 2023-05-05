81ca1c0ecc995fe5bdb69d9978dcbdec413e383d
Shawn Kelly <skellyb@gmail.com>
2023-05-05 10:12:59 -0400

    improve new chat tests

--- src/time.ts
+++ src/time.ts
@@ -1,8 +1,8 @@
 /**
  * Create a condensed UTC timestamp.
  */
-export function timestamp() {
-  const now = new Date();
+export function timestamp(date?: Date) {
+  const now = date ?? new Date();
   // from something like 2021-09-30T13:50:23.123Z to 20210930T135023
   return now.toISOString().replace(/[-:]|\.\d+Z$/g, "");
 }
--- tests/new_test.ts
+++ tests/new_test.ts
@@ -1,11 +1,13 @@
 import { assertEquals } from "https://deno.land/std@0.185.0/testing/asserts.ts";
+import { FakeTime } from "https://deno.land/std@0.185.0/testing/time.ts";
 import { newChat } from "../src/new.ts";
 import { timestamp } from "../src/time.ts";
 
-Deno.test("create new chat in current directory with timestamp filename", async (t) => {
-  const ts = timestamp();
+Deno.test("create new chat in current directory with timestamp filename", () => {
+  const time = new FakeTime(new Date("February 20, 2020 20:20:20"));
+  const ts = timestamp(new Date());
   const path = `chat-${ts}.toml`;
-  await t.step("create file", () => {
+  try {
     newChat("testing");
     const file = Deno.readFileSync(path);
     const txt = new TextDecoder().decode(file);
@@ -24,14 +26,17 @@ role = "user"
 content = """testing"""
 `,
     );
-  });
-  Deno.removeSync(path);
+  } finally {
+    Deno.removeSync(path);
+    time.restore();
+  }
 });
 
-Deno.test("create new chat at location with default timestamped filename", async (t) => {
-  const ts = timestamp();
+Deno.test("create new chat at location with default timestamped filename", () => {
+  const time = new FakeTime(new Date("February 20, 2020 20:20:20"));
+  const ts = timestamp(new Date());
   const path = `chats/chat-${ts}.toml`;
-  await t.step("create file", () => {
+  try {
     newChat("testing", "./chats");
     const file = Deno.readFileSync(path);
     const txt = new TextDecoder().decode(file);
@@ -50,13 +55,16 @@ role = "user"
 content = """testing"""
 `,
     );
-  });
-  Deno.removeSync(path);
+  } finally {
+    Deno.removeSync(path);
+    time.restore();
+  }
 });
 
-Deno.test("create new chat at location with provided filename without a message", async (t) => {
+Deno.test("create new chat at location with provided filename without a message", () => {
+  const time = new FakeTime(new Date("February 20, 2020 20:20:20"));
   const path = `./chats/test-chat.toml`;
-  await t.step("create file", () => {
+  try {
     newChat(undefined, path);
     const file = Deno.readFileSync(path);
     const txt = new TextDecoder().decode(file);
@@ -69,6 +77,8 @@ role = "system"
 content = "you are helpful, concise expert in many things"
 `,
     );
-  });
-  Deno.removeSync(path);
+  } finally {
+    Deno.removeSync(path);
+    time.restore();
+  }
 });
